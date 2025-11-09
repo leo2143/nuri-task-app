b2bfcded8a39f8d27387ec989dd38a7217712eca
Leonardo orellana <orellanaleonardo782@gmail.com>
2025-11-09 01:14:26 -0300

    fix: router paths

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -21,6 +21,7 @@ export const router = createBrowserRouter([
     element: <PublicLayout />,
     children: [
       {
+        path: "/",
         element: <PublicRoute />,
         children: [
           {
